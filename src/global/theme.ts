082099fa163addce2339f434d8a0cdbd3fae6eca
Renan Goulart <renan.goulart4@gmail.com>
2025-02-18 23:16:57 -0300

    feat: theme

---
+++ src/global/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primary: {
+      main: '#55CC44'
+    },
+    blue: {
+      main: '#2f95dc'
+    },
+    error: {
+      main: '#ec2d01'
+    }
+  }
+} as const;
