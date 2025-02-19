b28c2b887cd35cdc887649c84f9e9e9b00bd1373
Renan Goulart <renan.goulart4@gmail.com>
2025-02-18 23:15:43 -0300

    feat: providers and migrations

---
+++ src/app/_layout.tsx
@@ -0,0 +1,62 @@
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
+import { ActivityIndicator, SafeAreaView, Text, View } from 'react-native';
+import { Slot } from 'expo-router';
+import { useEffect } from 'react';
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import { openDatabaseSync, SQLiteProvider } from "expo-sqlite";
+import migrations from '../../drizzle/migrations';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import * as SplashScreen from 'expo-splash-screen';
+
+export {
+  ErrorBoundary,
+} from 'expo-router';
+
+const queryClient = new QueryClient()
+
+const databaseName = process.env.DB_NAME || 'goufinances.db';
+
+const expoDB = openDatabaseSync(databaseName);
+const db = drizzle(expoDB);
+
+SplashScreen.preventAutoHideAsync();
+
+export default function () {
+  const { success, error } = useMigrations(db, migrations);
+
+  useDrizzleStudio(expoDB);
+
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+  if (!success) {
+    return (
+      <ActivityIndicator style={{ flex: 1, alignSelf: 'center', justifyContent: 'center' }} />
+    );
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SQLiteProvider databaseName={databaseName} options={{ enableChangeListener: true }}>
+        <SafeAreaProvider>
+          <SafeAreaView style={{ flex: 1 }}>
+            <Slot />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </SQLiteProvider>
+    </QueryClientProvider>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+import { Redirect } from "expo-router";
+
+export default function () {
+  return (
+    <Redirect href='/(main)/home' />
+  )
+}
